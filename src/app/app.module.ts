b7430918228e0a1752de8a053f8956326f8af387
Bruna <brunazorzanello@gmail.com>
2022-05-31 09:44:10 -0300

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule} from "@angular/material/snack-bar";
+import { CarrinhoModule } from './carrinho/carrinho.module';
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NaoEncontradoComponent,
+    BarraPesquisaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    CarrinhoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
